8f17ad376f2612bc48e69058023cc30adaf7de3b
ale.co1989@gmail.com <Pot_Anglon89>
2016-04-26 10:48:24 +0200

    Refactored variable names Refactored static strings for player names

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -3,10 +3,10 @@ import java.util.Map;
 
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int scorePlayer1 = 0;
+    private int scorePlayer2 = 0;
+    private String namePlayer1;
+    private String namePlayer2;
 
     private static Map<Integer, String> SCORE_CODE = new HashMap<Integer, String>();
     static {
@@ -16,48 +16,43 @@ public class TennisGame1 implements TennisGame {
         SCORE_CODE.put(3, "Forty");
     }
 
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+    public TennisGame1(String namePlayer1, String namePlayer2) {
+        this.namePlayer1 = namePlayer1;
+        this.namePlayer2 = namePlayer2;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName == namePlayer1)
+            scorePlayer1 += 1;
         else
-            m_score2 += 1;
+            scorePlayer2 += 1;
     }
 
     public String getScore() {
         String score = "";
 
-        if (scoresAreEqual(m_score1, m_score2))
-        {
-            if( m_score1 < 3)
-                score = SCORE_CODE.get(m_score1) + "-All";
+        if (scoresAreEqual(scorePlayer1, scorePlayer2)) {
+            if( scorePlayer1 < 3)
+                score = SCORE_CODE.get(scorePlayer1) + "-All";
             else
                 score = "Deuce";
         }
-        else if (atLeastOneScoreGreaterThanThree(m_score1, m_score2))
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                score = SCORE_CODE.get(m_score1) + "-" + SCORE_CODE.get(m_score2) ;
+        else if (atLeastOneScoreGreaterThanThree(scorePlayer1, scorePlayer2)) {
+            int minusResult = scorePlayer1 - scorePlayer2;
+            if (minusResult==1) score ="Advantage " + namePlayer1;
+            else if (minusResult ==-1) score ="Advantage " + namePlayer2;
+            else if (minusResult>=2) score = "Win for " + namePlayer1;
+            else score ="Win for " + namePlayer2;
+        }else {
+            for (int i = 1; i < 3; i++) {
+                score = SCORE_CODE.get(scorePlayer1) + "-" + SCORE_CODE.get(scorePlayer2) ;
             }
         }
         return score;
     }
 
     private boolean scoresAreEqual(int score1, int score2) {
-        return (score1==score2);
+        return (score1 == score2);
     }
 
     private boolean atLeastOneScoreGreaterThanThree(int score1, int score2) {
